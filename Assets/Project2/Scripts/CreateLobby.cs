28b5e58401b130523ac31423f0a14202f621582f
tyceo <tyceo87@gmail.com>
2026-04-14 14:22:11 +1000

    relay is now working

--- Assets/Project2/Scripts/CreateLobby.cs
+++ Assets/Project2/Scripts/CreateLobby.cs
@@ -4,6 +4,13 @@ using Unity.Services.Lobbies.Models;
 using Unity.Services.Authentication;
 using System.Threading.Tasks;
 
+//relay
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using System.Collections.Generic;
+
 public class CreateLobby : MonoBehaviour
 {
     private Lobby currentLobby;
@@ -32,7 +39,32 @@ public class CreateLobby : MonoBehaviour
 
         Debug.Log("Lobby created Code: " + currentLobby.LobbyCode);
 
-        //start heartbeat loop
+        //relay and start host netcode
+
+        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxPlayers);
+        string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+        var transport = NetworkManager.Singleton.GetComponent<Unity.Netcode.Transports.UTP.UnityTransport>();
+        transport.SetRelayServerData(allocation.ToRelayServerData("dtls"));
+
+        NetworkManager.Singleton.StartHost();
+
+        //store relay code in lobby
+        var updateOptions = new UpdateLobbyOptions
+        {
+            Data = new Dictionary<string, DataObject>
+            {
+                {
+                    "RelayCode",
+                    new DataObject(DataObject.VisibilityOptions.Public, joinCode)
+                }
+            }
+        };
+
+        await LobbyService.Instance.UpdateLobbyAsync(currentLobby.Id, updateOptions);
+
+        Debug.Log("Relay started + Host running");
+
         StartCoroutine(HeartbeatLoop());
     }
 
--- Assets/Project2/Scripts/JoinLobby.cs
+++ Assets/Project2/Scripts/JoinLobby.cs
@@ -4,6 +4,12 @@ using Unity.Services.Lobbies.Models;
 using Unity.Services.Authentication;
 using TMPro;
 
+//relay
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+
 public class JoinLobby : MonoBehaviour
 {
     public TMP_InputField inputField; //inspector
@@ -21,5 +27,21 @@ public class JoinLobby : MonoBehaviour
         Lobby lobby = await LobbyService.Instance.JoinLobbyByCodeAsync(lobbyCode);
 
         Debug.Log("Joined lobby: " + lobby.Name);
+
+        //relay and client
+
+        string relayCode = lobby.Data["RelayCode"].Value;
+
+        JoinAllocation joinAllocation =
+            await RelayService.Instance.JoinAllocationAsync(relayCode);
+
+        var transport =
+            NetworkManager.Singleton.GetComponent<Unity.Netcode.Transports.UTP.UnityTransport>();
+
+        transport.SetRelayServerData(joinAllocation.ToRelayServerData("dtls"));
+
+        NetworkManager.Singleton.StartClient();
+
+        Debug.Log("Connected to Relay + Client started");
     }
 }
